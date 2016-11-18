487f4c4424a4ca0ceaa9321a1807be3f05a80418
David Fallah <davidfallah@atp-group.com>
2016-11-18 13:39:36 +0000

    Add regex-based incoming message filter

--- test/Mofichan.Tests/BaseReflectionBehaviourTests.cs
+++ test/Mofichan.Tests/BaseReflectionBehaviourTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive;
+using System.Text.RegularExpressions;
 using Mofichan.Behaviour.Base;
+using Mofichan.Behaviour.FilterAttributes;
 using Mofichan.Core;
 using Mofichan.Core.Interfaces;
 using Moq;
@@ -48,16 +51,26 @@ namespace Mofichan.Tests
             [MockIncomingMessageFilter(permitMessage: false)]
             public OutgoingMessage? Should_Not_Receive_Message(IncomingMessage message)
             {
-                return ReflectBackMessageWithSuffix(message, " <<shouldn't receive>>");
+                return ReflectBackMessageWithSuffix(message, " baz");
+            }
+            
+            [RegexIncomingMessageFilter(regex: "abc(?=def)", options: RegexOptions.IgnoreCase)]
+            public OutgoingMessage? Should_Invoke_On_Match(IncomingMessage message)
+            {
+                return BuildMessageWithBody("Matched: " + message.Context.Body);
             }
 
-            private OutgoingMessage ReflectBackMessageWithSuffix(IncomingMessage message, string suffix)
+            private static OutgoingMessage ReflectBackMessageWithSuffix(IncomingMessage message, string suffix)
+            {
+                return BuildMessageWithBody(message.Context.Body + suffix);
+            }
+
+            private static OutgoingMessage BuildMessageWithBody(string body)
             {
                 var mockTarget = Mock.Of<IMessageTarget>();
-                var newBody = message.Context.Body + suffix;
-                var newContext = new MessageContext(mockTarget, mockTarget, newBody);
+                var context = new MessageContext(mockTarget, mockTarget, body);
 
-                return new OutgoingMessage { Context = newContext };
+                return new OutgoingMessage { Context = context };
             }
         }
         #endregion
@@ -81,9 +94,54 @@ namespace Mofichan.Tests
             behaviour.OnNext(message);
 
             // THEN the methods within it should have been invoked based on their applied attributes.
-            responses.Count.ShouldBe(2);
             responses.ShouldContain("hello, foo");
             responses.ShouldContain("hello, bar");
+            responses.ShouldNotContain("hello, baz");
+        }
+
+        [Fact]
+        public void Reflection_Behaviour_Should_Support_Regex_Based_Filtering()
+        {
+            // GIVEN an instance of a mock reflection behaviour.
+            var behaviour = new MockReflectionBehaviour();
+
+            // GIVEN a set of incoming messages
+            Func<string, IncomingMessage> messageBuilder = message =>
+            {
+                var mockTarget = Mock.Of<IMessageTarget>();
+                var messageContext = new MessageContext(mockTarget, mockTarget, message);
+                return new IncomingMessage(messageContext);
+            };
+
+            var abc = messageBuilder("abc");
+            var abcdef = messageBuilder("abcdef");
+            var aBcDeF = messageBuilder("aBcDeF");
+
+            // GIVEN we are subscribed to responses from the behaviour.
+            var responses = new List<string>();
+            var observer = Observer.Create<OutgoingMessage>(response => responses.Add(response.Context.Body));
+            behaviour.Subscribe(observer);
+
+            // WHEN we pass "abc" to the observer.
+            behaviour.OnNext(abc);
+
+            // THEN we should not have received a "matched" response.
+            responses.ShouldNotContain("Matched: abc");
+            responses.Clear();
+
+            // WHEN we pass "abcdef" to the observer.
+            behaviour.OnNext(abcdef);
+
+            // THEN we should have received a "matched" response.
+            responses.ShouldContain("Matched: abcdef");
+            responses.Clear();
+
+            // WHEN we pass "aBcDeF" to the observer.
+            behaviour.OnNext(aBcDeF);
+
+            // THEN we should have received a "matched" response.
+            responses.ShouldContain("Matched: aBcDeF");
+            responses.Clear();
         }
     }
 }
