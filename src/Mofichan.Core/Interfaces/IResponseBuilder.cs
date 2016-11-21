00fb6a33f7f23456cc74891caae0a891843f558c
David Fallah <davidfallah@atp-group.com>
2016-11-21 11:44:59 +0000

    Add initial article tag logic
    Article tags allow Mofichan to choose among a
    set of possible responses based on tags

---
+++ src/Mofichan.Core/Interfaces/IResponseBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace Mofichan.Core.Interfaces
+{
+    public interface IResponseBuilder
+    {
+        IResponseBuilder With(params string[] tags);
+        IResponseBuilder With(double chance, params string[] tags);
+
+        IResponseBuilder WithAnyOf(params string[] phrases);
+        IResponseBuilder WithAnyOf(double chance, params string[] phrases);
+
+        string Build();
+    }
+}
---
+++ src/Mofichan.Library/LibraryModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Autofac;
+
+namespace Mofichan.Library
+{
+    public class LibraryModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
